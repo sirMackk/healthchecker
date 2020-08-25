be96eee7ab61d1230c36714148dce7200a5b3d43
sirMackk <c.tairix@gmail.com>
2020-08-25 12:39:23 -0800

    refactor httpChecks; add checkRegistry

--- health_checks.go
+++ health_checks.go
@@ -5,6 +5,30 @@ import (
 	"fmt"
 )
 
+type CheckRegistry struct {
+	CheckFuncs map[string]func(map[string]string) (func() *CheckResult)
+	Checks []*HealthCheck
+}
+
+func NewCheckRegistry() *CheckRegistry {
+	registry := CheckRegistry{}
+	// TODO: set timeout from config
+	httpChecker := NewHTTPChecker(10 * time.Second)
+	registry.CheckFuncs["SimpleHTTPCheck"] = httpChecker.NewSimpleHTTPCheck
+	registry.CheckFuncs["RegexpHTTPCheck"] = httpChecker.NewRegexpHTTPCheck
+	return &registry
+}
+
+func (c *CheckRegistry) NewCheck(name string, args map[string]string) *HealthCheck {
+	hc := HealthCheck{
+		check: c.CheckFuncs[name](args),
+		sink: nil,
+	}
+	c.Checks = append(c.Checks, &hc)
+	return &hc
+}
+
+
 type CheckResult struct {
 	Timestamp time.Time
 	Name      string
@@ -14,4 +38,10 @@ type CheckResult struct {
 
 func (c *CheckResult) TimestampString() string {
 	return fmt.Sprintf(c.Timestamp.Format("2006-01-02 15:04:05.999999"))
+}
+
+
+type HealthCheck struct {
+	check func() *CheckResult
+	sink []*Sink
 }
