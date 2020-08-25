718a6755adb9851815b0218c9ff6969ad9df518e
sirMackk <matt@mattscodecave.com>
2020-08-25 15:27:41 -0800

    Fix yaml config reading

--- config_reader_test.go
+++ config_reader_test.go
@@ -1,31 +1,10 @@
 package healthchecker
 
 import (
+	"fmt"
 	"io/ioutil"
-	//"reflect"
 	"testing"
 )
-
-//func TestSimpleJsonConfigRead(t *testing.T) {
-	//exampleJson, _ := ioutil.ReadFile("fixtures/basicConfig.json")
-	//config := ConfigFromJson(exampleJson)
-	//if config.Core["timeout"] != "10" {
-		//t.FailNow()
-	//}
-
-	//coreMap := map[string]string{"timeout": "10"}
-	//if !reflect.DeepEqual(config.Core, coreMap) {
-		//t.FailNow()
-	//}
-
-	//healthcheckMap := map[string]map[string]string{
-		//"basic":    {"timeout": "5", "target": "http://example.com"},
-		//"advanced": {"timeout": "7", "target": "https://example.com"},
-	//}
-	//if !reflect.DeepEqual(config.HealthChecks, healthcheckMap) {
-		//t.FailNow()
-	//}
-//}
 
 func TestSimpleYamlConfigRead(t *testing.T) {
 	exampleYaml, _ := ioutil.ReadFile("fixtures/exampleConfig.yaml")
@@ -33,4 +12,5 @@ func TestSimpleYamlConfigRead(t *testing.T) {
 	if config.Core["HTTPTimeout"] != "10" {
 		t.Errorf("Fail: %s\n%v", exampleYaml, config)
 	}
+	// TODO add finer check
 }
