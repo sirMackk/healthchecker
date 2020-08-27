d4ccdefbd89f0999e3c5b52cb1497fd9506da34f
sirMackk <c.tairix@gmail.com>
2020-08-27 09:23:11 -0800

    Refactor icmp check; Add tests

--- net_checks.go
+++ net_checks.go
@@ -1,20 +1,31 @@
 package healthchecker
 
 import (
+	"fmt"
 	"net"
 	"time"
 
 	"golang.org/x/net/icmp"
 	"golang.org/x/net/ipv4"
+	"golang.org/x/net/ipv6"
 
 	log "github.com/sirupsen/logrus"
 )
 
-// TODO: refactor ICMPV4Check
-// TODO: add constructor functions to link up with main checker
+type ICMPPacketConn interface {
+	Close() error
+	IPv4PacketConn() *ipv4.PacketConn
+	IPv6PacketConn() *ipv6.PacketConn
+	LocalAddr() net.Addr
+	ReadFrom(b []byte) (int, net.Addr, error)
+	WriteTo(b []byte, dst net.Addr) (int, error)
+	SetDeadline(t time.Time) error
+	SetReadDeadline(t time.Time) error
+	SetWriteDeadline(t time.Time) error
+}
 
 type ICMPChecker struct {
-	Conn *icmp.PacketConn
+	Conn ICMPPacketConn
 }
 
 func NewICMPChecker(timeout time.Duration) (*ICMPChecker, error) {
@@ -29,63 +40,70 @@ func NewICMPChecker(timeout time.Duration) (*ICMPChecker, error) {
 	return &checker, nil
 }
 
-
-func (i *ICMPChecker) ICMPV4Check(targetIP *net.IPAddr) *CheckResult {
-	timeStart := time.Now()
+func (i *ICMPChecker) sendICMPV4Echo(targetIP *net.IPAddr, ua []byte) (*icmp.Message, error) {
 	echoReq := icmp.Message{
 		Type: ipv4.ICMPTypeEcho,
 		Code: 0,
 		Body: &icmp.Echo{
-			ID: 0,
-			Seq: 1,
-			Data: []byte("sirmackk/healthchecker"),
+			ID:   0,
+			Seq:  1,
+			Data: ua,
 		},
 	}
-	encodedReq, _ := echoReq.Marshal(nil)
-	_, err := i.Conn.WriteTo(encodedReq, targetIP)
+	encodedReq, err := echoReq.Marshal(nil)
 	if err != nil {
-		return &CheckResult{
-			Timestamp: timeStart,
-			Result: Failure,
-			Duration: time.Since(timeStart),
-		}
+		log.Debugf("ICMP check failed: couldn't marshal echo req: %s", err)
+		return nil, err
+	}
+	_, err = i.Conn.WriteTo(encodedReq, targetIP)
+	if err != nil {
+		log.Debugf("ICMP check failed, couldn't write to conn: %s", err)
+		return nil, err
 	}
 
-	// TODO do I need to check the peer?
-	buf := make([]byte, 2048)
+	buf := make([]byte, 256)
 	bytesRead, _, err := i.Conn.ReadFrom(buf)
 	if err != nil {
 		log.Debugf("ICMP check failed, couldn't read from conn: %v", err)
-		return &CheckResult{
-			Timestamp: timeStart,
-			Result: Failure,
-			Duration: time.Since(timeStart),
-		}
+		return nil, err
 	}
 
-	resp, err := icmp.ParseMessage(58, buf[:bytesRead])
+	resp, err := icmp.ParseMessage(1, buf[:bytesRead])
 	if err != nil {
 		log.Debugf("ICMP check failed, couldn't parse reply: %v - %v", err, buf[:bytesRead])
+		return nil, err
+	}
+	return resp, nil
+}
+
+func (i *ICMPChecker) ICMPV4Check(targetIP *net.IPAddr) *CheckResult {
+	timeStart := time.Now()
+	resp, err := i.sendICMPV4Echo(targetIP, []byte("sirmackk/healthchecker"))
+	if err != nil || resp.Type != ipv4.ICMPTypeEchoReply {
 		return &CheckResult{
 			Timestamp: timeStart,
-			Result: Failure,
-			Duration: time.Since(timeStart),
+			Result:    Failure,
+			Duration:  time.Since(timeStart),
 		}
 	}
 
-	switch resp.Type {
-	case ipv4.ICMPTypeEchoReply:
-		return &CheckResult{
-			Timestamp: timeStart,
-			Result: Success,
-			Duration: time.Since(timeStart),
-		}
-	default:
-		log.Debugf("ICMP check failed: %v - %v", targetIP, resp.Type)
-		return &CheckResult{
-			Timestamp: timeStart,
-			Result: Failure,
-			Duration: time.Since(timeStart),
-		}
+	return &CheckResult{
+		Timestamp: timeStart,
+		Result:    Success,
+		Duration:  time.Since(timeStart),
 	}
+}
+
+func (i *ICMPChecker) NewICMPV4Check(args map[string]string) (func() *CheckResult, error) {
+	IP, ok := args["targetIP"]
+	if !ok {
+		return nil, fmt.Errorf("ICMPV4Check missing 'targetIP' parameter")
+	}
+	targetIP, err := net.ResolveIPAddr("ip4", IP)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to parse %s into ipv4 address: %s", IP, err)
+	}
+	return func() *CheckResult {
+		return i.ICMPV4Check(targetIP)
+	}, nil
 }
---
+++ net_checks_test.go
@@ -0,0 +1,83 @@
+package healthchecker
+
+import (
+	"net"
+	"testing"
+	"time"
+
+	"golang.org/x/net/ipv4"
+	"golang.org/x/net/ipv6"
+)
+
+type fakePacketConn struct {
+	InputBuf  []byte
+	OutputBuf []byte
+	Addr      net.Addr
+}
+
+func (f fakePacketConn) ReadFrom(p []byte) (n int, addr net.Addr, err error) {
+	n = copy(p, f.OutputBuf)
+	return n, f.Addr, nil
+}
+
+func (f fakePacketConn) WriteTo(p []byte, addr net.Addr) (n int, err error) {
+	n = copy(f.InputBuf, p)
+	return n, nil
+}
+
+func (f fakePacketConn) LocalAddr() net.Addr {
+	addr, _ := net.ResolveTCPAddr("tcp", "127.0.0.1")
+	return addr
+}
+
+func (f fakePacketConn) Close() error                       { return nil }
+func (f fakePacketConn) SetDeadline(t time.Time) error      { return nil }
+func (f fakePacketConn) SetReadDeadline(t time.Time) error  { return nil }
+func (f fakePacketConn) SetWriteDeadline(t time.Time) error { return nil }
+func (f fakePacketConn) IPv4PacketConn() *ipv4.PacketConn   { return nil }
+func (f fakePacketConn) IPv6PacketConn() *ipv6.PacketConn   { return nil }
+
+func NewFakePacketConn() ICMPPacketConn {
+	addr, _ := net.ResolveTCPAddr("tcp", "127.0.0.1")
+	return &fakePacketConn{
+		InputBuf:  make([]byte, 32),
+		OutputBuf: nil,
+		Addr:      addr,
+	}
+}
+
+func TestICMPV4Check(t *testing.T) {
+	OutputMsgs := []struct {
+		result Outcome
+		outbuf []byte
+	}{
+		{Success, []byte{
+			0x0, 0x0, 0xb7, 0xd2, 0x3, 0xe9, 0x0, 0x1, 0x70, 0x69, 0x6e, 0x67, 0x65, 0x72}},
+		{Failure, []byte{
+			0xFF, 0x65, 0x72}},
+		{Failure, []byte{
+			0x8, 0x0, 0xaf, 0xd2, 0x3, 0xe9, 0x0, 0x1, 0x70, 0x69, 0x6e, 0x67, 0x65, 0x72}},
+	}
+
+	for _, tt := range OutputMsgs {
+		t.Run(string(tt.result), func(t *testing.T) {
+			fakePktConn := NewFakePacketConn()
+			fakePktConn.(*fakePacketConn).OutputBuf = tt.outbuf
+			checker := &ICMPChecker{
+				Conn: fakePktConn,
+			}
+
+			checkFunc, _ := checker.NewICMPV4Check(map[string]string{"targetIP": "localhost"})
+			res := checkFunc()
+
+			if res.Result != tt.result {
+				t.Errorf(
+					"Got: %#v, Wanted: %#v\nInputBuf: %#v",
+					res.Result,
+					tt.result,
+					fakePktConn.(*fakePacketConn).InputBuf,
+				)
+			}
+		})
+	}
+}
